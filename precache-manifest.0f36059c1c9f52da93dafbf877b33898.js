de8cd013088f0523f764b42574128a101f51b01d
João Luiz <mac@MacBook-Air-de-mac.local>
2020-07-20 07:02:57 -0300

    Updates

---
+++ precache-manifest.0f36059c1c9f52da93dafbf877b33898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7963493ebcf7f061e033ebb4427eab9d",
+    "url": "/Articles-Search/index.html"
+  },
+  {
+    "revision": "49acb1425c9da17ee28d",
+    "url": "/Articles-Search/static/css/main.65936af3.chunk.css"
+  },
+  {
+    "revision": "50b670e2b662d80cd6da",
+    "url": "/Articles-Search/static/js/2.2b60e0f9.chunk.js"
+  },
+  {
+    "revision": "6020e2578ce61e153ee5130f44d173fc",
+    "url": "/Articles-Search/static/js/2.2b60e0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49acb1425c9da17ee28d",
+    "url": "/Articles-Search/static/js/main.f17eb499.chunk.js"
+  },
+  {
+    "revision": "16c6534b37d249c46564",
+    "url": "/Articles-Search/static/js/runtime-main.99275668.js"
+  }
+]);
